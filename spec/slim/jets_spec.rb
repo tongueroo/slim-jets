3933f7095e234d7533c2aaa67c155da90bcf7411
Tung Nguyen <tongueroo@gmail.com>
2019-06-01 05:23:32 +0000

    init commit

---
+++ spec/slim/jets_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Slim::Jets do
+  it "has a version number" do
+    expect(Slim::Jets::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
